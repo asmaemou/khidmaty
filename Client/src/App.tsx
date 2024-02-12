b651ab99ef3d0c260327dc630c76ed3622046eb1
asmaemou <amouradi2002@gmail.com>
2024-02-12 11:56:40 +0100

    sign In
    add sign in of fatima

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 import SignUp1 from './pages/SignUp1';
 import SignUp2 from './pages/SignUp2';
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/signup1" element={<SignUp1 />} />
           <Route path="/signup2" element={<SignUp2 />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </div>
     </Router>
---
+++ Client/src/pages/SignIn.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignInPage from '../Components/ReactComponents/SignInPage'
+
+
+const SignIn: React.FC = () => {
+  return (
+    <div>
+     <SignInPage/>
+    </div>
+  );
+};
+
+export default SignIn;
